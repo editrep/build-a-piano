f255cbf4cab269b4ec7fd81b1ff961b8b1e424e3
editrep <edithedowney@icloud.com>
2026-08-29 15:48:06 -0400

    added extra credit automation: plays a verse of golden from kpdh

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const blackKeys = document.querySelectorAll('.key.black');
 
 //selects all keys
 const keys = document.querySelectorAll('.key');
+const playSongBtn = document.getElementById('playSong');
 
 
 // --Liseners --
@@ -55,3 +56,25 @@ document.addEventListener('keydown', (e) => {
     playNote(blackKeys[blackKeyIndex]);
   }
 });
+
+// -- Extra Credit: Automating a Song --
+// inspiration from Exemplar Submissions on Cavas (Jonathan Netala, Spring 2025 | Twinkle, Twinkle, Little Star)
+// song notes from https://www.mintmusic.co.uk/2025/09/golden-huntrx-kpop-demon-hunters.html
+const song = [ 'G', 'A', 'B', 'B', 'G', 'A', 'B', 'B', 'D', 'G', 'G', 'A', 'D', 'G', 'G', 'F', 'E', 'E', 'F', 'G', 'G', 'A', 'B', 'B', 'G', 'A', 'B', 'B', 'D', 'G', 'G', 'A', 'F', 'E', 'F', 'F', 'E', 'F', 'E', 'E', 'D', 'D', 'A','A', 'A', 'D', 'D', 'A', 'A', 'A', 'G', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'D', 'D', 'A', 'A', 'A', 'D', 'D', 'A', 'A', 'A', 'B', 'D', 'A', 'A', 'A', 'A', 'G', 'F', 'F', 'E', 'D', 'E', 'E', 'E'];
+    // 'E', 'G', 'C', 'B', 'D', 'G', 'E', 'D', 'F', 'A', 'D', 'C', 'B', 'F', 'E', 'G', 'C', 'B', 'D', 'G', 'E', 'D', 'F', 'A', 'D', 'B', 'B', 'E', 'E', 'E', 'E', 'B', 'B', 'D', 'D', 'A', 'D', 'D', 'D', 'D', 'C', 'C', 'C', 'B', 'D', 'D', 'D', 'C', 'C', 'B', 'A', 'B', 'G', 'E', 'E', 'G', 'F', 'E', 'D', 'D'];
+let songIndex = 0;
+
+playSongBtn.addEventListener('click', () => {
+  songIndex = 0;
+  playNextNote();
+});
+
+function playNextNote() {
+  if (songIndex < song.length) {
+    const key = document.querySelector(`[data-note="${song[songIndex]}"]`);
+    playNote(key);
+
+    songIndex++;
+    setTimeout(playNextNote, 500); // Adjust timing between notes
+  }
+}
